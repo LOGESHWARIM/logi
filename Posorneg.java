3a71ef7a80c566d9243c4a393c4f147bdf6dc5ef
LOGESHWARIM <47376868+LOGESHWARIM@users.noreply.github.com>
2019-02-05 22:20:54 -0800

    Create Posorneg.java

---
+++ Posorneg.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Posorneg
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number=-1;
+		if( number > 0)
+		System.out.println("Positive");
+		else if(number < 0)
+		System.out.println("Negative");
+		else
+		System.out.println("Zero");
+	}
+}
